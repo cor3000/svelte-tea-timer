754e34bbdd570d9a1c10dcd323fb8ae0d0fb7512
cor3000 <cor3000@web.de>
2019-06-26 08:36:42 +0800

    initial

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Brew from './Brew.svelte';
+	export let title;
+	let brewTimes = [80, 40, 50, 60, 80, 100];
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>{title}</h1>
+<div>
+	{#each brewTimes as duration}
+		<Brew {duration}/>
+	{/each}
+</div>
